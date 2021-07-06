087723105818e55e9a7e39e9993fa0762098f6ea
dileep pal <neverbackdown.dilip@gmail.com>
2021-07-06 21:59:42 +0530

    todo first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="col-4">
+            
+        </div>
+        <div className="col-4 maindiv">
+              <TodoList />
+        </div>
+        <div className="col-4">
+              
+        </div>
+      </div>
+        
+    </div>
+  );
+}
+
+export default App;
